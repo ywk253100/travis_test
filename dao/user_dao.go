6d77b34d19809f7394738b3af0ad0c7f4b1f0505
yinw <yinw@vmware.com>
2016-02-24 15:44:40 +0800

    go vet

--- dao/user_dao.go
+++ dao/user_dao.go
@@ -1,11 +1,16 @@
 package dao
 
 import (
+	"fmt"
+)
+
+import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func GetAllUser() ([]string, error) {
+	fmt.Printf("%s %d")
 	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/registry")
 	if err != nil {
 		return nil, err
